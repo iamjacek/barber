381f9960056d99055d9f01ca5f7c9275319cea58
Jacek <iamjacek@icloud.com>
2018-12-20 12:45:18 +0000

    upload configured webpack and index page

--- src/index.js
+++ src/index.js
@@ -1,3 +1,63 @@
+import $ from 'jquery';
+import PopperJs from './js/popper.min.js';
+import './js/jquery.min.js';
 
-import img from './img/big.jpg';
+import './js/bootstrap.min.js';
+
+//************ FONTS ****************/
+import font1 from './fonts/bernier.ttf';
+
+//************ IMAGES ****************/
+import img from './img/arrow_active.png';
+import img2 from './img/arrow.png';
+import img3 from './img/banner.png';
+import img4 from './img/barber1.png';
+import img5 from './img/barber2.png';
+import img6 from './img/barber3.png';
+import img7 from './img/dom.png';
+import img8 from './img/fb.png';
+import img9 from './img/follow.png';
+import img10 from './img/galeria.png';
+import img11 from './img/i.png';
+import img12 from './img/insta.png';
+import img13 from './img/left.png';
+import img14 from './img/logo.png';
+import img15 from './img/noz.png';
+import img16 from './img/noze_close.png';
+import img17 from './img/noze_open.png';
+import img18 from './img/noze.png';
+import img19 from './img/right.png';
+import img20 from './img/slider1.png';
+import img21 from './img/slider2.png';
+import img22 from './img/slider3.png';
+import img23 from './img/slider4.png';
+import img24 from './img/tekstura.png';
+import img25 from './img/twitter.png';
+
+//************** HTML ****************/
+import index from './index.html';
+
+//************** CSS ****************/
+import stylebootstrap from './css/bootstrap.css';
+import stylefontawesome from './css/font-awesome.min.css';
+
+//************** STYLE ****************/
 import style from "./_scss/main.scss";
+
+
+onload = function () {
+    $('.card-body img').hover(function() {
+        $(this).attr('src', './img/noze_close.png');
+      }, function() {
+        $(this).attr('src', './img/noze_open.png');
+      })
+
+      $('.card-body span').hover(function() {
+        $('.card-body img').attr('src', './img/noze_close.png');
+      }, function() {
+        $('.card-body img').attr('src', './img/noze_open.png');
+      })
+}
+
+
+
--- webpack.config.js
+++ webpack.config.js
@@ -1,91 +1,115 @@
 const CleanWebpackPlugin = require('clean-webpack-plugin');
-const HtmlWebPackPlugin = require("html-webpack-plugin");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+var webpack = require('webpack');
 var path = require('path');
 
 
 
 module.exports = {
-    devServer: {
-        watchContentBase: true,
-        contentBase: path.join(__dirname, 'dist'),
-        compress: true,   
-        port: 9000
-      },
-    module: {
-        rules: [
-          {
-            test: /\.m?js$/,
-            exclude: /(node_modules|bower_components)/,
-            use: {
-              loader: 'babel-loader',
+  entry: './src/index.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: './js/bundle.js'
+  },
+  devServer: {
+      watchContentBase: true,
+      contentBase: path.join(__dirname, 'dist'),
+      compress: true,
+      port: 9000
+    },
+  module: {
+      rules: [
+
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        },
+        {
+          test: /\.scss$/,
+          use: [
+              MiniCssExtractPlugin.loader,
+              "css-loader?url=false",
+              "postcss-loader",
+              "sass-loader"
+          ]
+        },
+        {
+          test: /.*\.(gif|png|jpe?g)$/i,
+          use: [
+            {
+              loader: 'file-loader',
               options: {
-                presets: ['@babel/preset-env']
+                name: './img/[name].[ext]',
+                context: ''
+              }
+            },
+            {
+              loader: 'image-webpack-loader',
+              options: {
+                optipng: {
+                  optimizationLevel: 7
+                },
+                pngquant: {
+                  quality: '65-90'
+                },
+                mozjpeg: {
+                  quality: 65
+                }
               }
             }
-          },
-          {
-            test: /\.html$/,
-            use: [{
-                loader: "html-loader",
-                options: {
-                    minimize: true
-                }
-            }]
-          },
-          {
-            test: /\.scss$/,
-            use: [
-                MiniCssExtractPlugin.loader,
-                "css-loader",
-                "postcss-loader",
-                "sass-loader"
-            ]
-          },
-          {
-            test: /.*\.(gif|png|jpe?g)$/i,
-            use: [
-              {
-                loader: 'url-loader',
-                options: {
-                  limit: 8000,
-                  name: '/images/[name]_[sha512:hash:base64:7].[ext]'
-                }
-              },
-              {
-                loader: 'image-webpack-loader',
-                options: {
-                  optipng: {
-                    optimizationLevel: 7
-                  },
-                  pngquant: {
-                    quality: '65-90'
-                  },
-                  mozjpeg: {
-                    quality: 65
-                  }
-                }
-              }
-            ]
+          ]
 
-          },
-        ],
-    },
+        },
+        {
+          test: /\.html$/,
+          use:{
+            loader: 'file-loader',
+            options: {
+              name: './[name].[ext]',
+              context: ''
+            }
+          }
+        },
+        {
+          test: /\.css$/,
+          use:{
+            loader: 'file-loader',
+            options: {
+              name: './css/[name].[ext]',
+              context: ''
+            }
+          }
+        },
+        {
+          test: /\.ttf$/,
+          use:{
+            loader: 'file-loader',
+            options: {
+              name: './fonts/[name].[ext]',
+              context: ''
+            }
+          }
+        }
+      ]
+  },
 
 
       plugins: [
         new CleanWebpackPlugin(['dist']),
-        new HtmlWebPackPlugin({
-          template: "src/index.html",
-          filename: "./index.html"
-        }),
-        new HtmlWebPackPlugin({
-            template: "src/about.html",
-            filename: "./about.html"
-        }),
             new MiniCssExtractPlugin({
-        filename: "[name].css",
+        filename: "./css/[name].css",
         chunkFilename: "[id].css"
+        }),
+        new webpack.ProvidePlugin({
+          $: 'jquery',
+          jQuery: 'jquery',
+          Popper: 'popper.js'
         })
       ]
 
